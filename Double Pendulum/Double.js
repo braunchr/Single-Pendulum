a0d8ad567044784f40fc5fc51b0f828d197ccace
braunchr <christophebraun@hotmail.com>
2017-10-21 12:36:19 +0100

    single pendulum with comments

--- Double Pendulum/Double.js
+++ Double Pendulum/Double.js
@@ -5,18 +5,27 @@ function init() {
     c = document.getElementById("canvas1");
     ctx = c.getContext("2d");
 
+    PLength = 0.24;
+    theta = 2*Math.PI/4 ;
+    theta1 = 0;
+    theta2 = 0;
+    deltat = 1 / 60;
+
+    g = 9.81;
+
     ax = new Axis(c, ctx);
-    
+
+    // this is the postion of the origin of hte pendulum
     b1 = new Ball();
-    b1.VX = 0.03;
-    b1.VY = 0.025;
     b1.BallColor = "Green";
+    b1.Y = 0;
+    b1.Radius = 8;
 
     b2 = new Ball();
-    b2.VX = 0.025;
-    b2.VY = 0.02;
-    
- 
+    b2.X = PLength * Math.sin(theta);
+    b2.Y = -PLength * Math.cos(theta) + b1.Y;
+    b2.Radius = 15;
+
     Draw();
 }
 
@@ -24,18 +33,16 @@ function init() {
 // Functionto be called by animation timer.
 function Draw() {
 
+    //The differential equation goes here
+    theta2 = -g / PLength * Math.sin(theta);
 
-    b1.X += b1.VX;
-    if (b1.X > ax.XaxisMax || b1.X < ax.XaxisMin) { b1.VX = -b1.VX }
+    //These are the definitions of the derivatives iterations
+    theta1 = (theta1 + theta2 * deltat) ; 
+    theta = (theta + theta1 * deltat);
 
-    b1.Y += b1.VY;
-    if (b1.Y > ax.YaxisMax || b1.Y < ax.YaxisMin) { b1.VY = -b1.VY }
-
-    b2.X += b2.VX;
-    if (b2.X > ax.XaxisMax || b2.X < ax.XaxisMin) { b2.VX = -b2.VX }
-
-    b2.Y += b2.VY;
-    if (b2.Y > ax.YaxisMax || b2.Y < ax.YaxisMin) { b2.VY = -b2.VY }
+    //Convert the new polar coordinates in cartesian coordinates
+    b2.X = PLength * Math.sin(theta);
+    b2.Y = -PLength * Math.cos(theta)+b1.Y;
 
 
 
@@ -47,7 +54,7 @@ function Draw() {
     b1.Draw();
     b2.Draw();
 
-    requestAnimationFrame(Draw);
+    re = requestAnimationFrame(Draw);
     
 }
 
@@ -56,13 +63,12 @@ function Ball() {
     // Starting position.
     this.X = 0 ;
     this.Y = 0 ;
-    this.BallColor = color1.value;
-    // Initial velocity.
-    this.VX =0;
-    this.VY = 0;
 
+    //Color of the ball (from the html object tagged color1)
+    this.BallColor = color1.value;
+    
     // Size of the ball.
-    this.Radius = 15;
+    this.Radius = 8;
 
    
 
@@ -93,6 +99,7 @@ Ball.prototype.Join = function (b2) {
 
 
 function reset() {
+    cancelAnimationFrame(re);
    init();
 
 }
@@ -100,18 +107,18 @@ function reset() {
 
 function Axis(c, ctx) {
     // Coordinates of the axis
-    this.XaxisMax = 2;
-    this.XaxisMin = -2;
-    this.YaxisMax = 2;
-    this.YaxisMin = -2;
+    this.XMax = 0.3;
+    this.XMin = -0.3;
+    this.YMax = 0.3;
+    this.YMin = -0.3;
     this.c = c;
     this.ctx = ctx;
 }
 
 Axis.prototype.ConvX = function (X) {
-    return (X * this.c.width / (this.XaxisMax - this.XaxisMin) + this.c.width / 2);    
+    return (X * this.c.width / (this.XMax - this.XMin) + this.c.width / 2);    
 }
 
 Axis.prototype.ConvY = function (Y) {
-    return (-Y * this.c.height / (this.YaxisMax - this.YaxisMin) + this.c.height / 2);
+    return (-Y * this.c.height / (this.YMax - this.YMin) + this.c.height / 2);
 }
